4a4770b7b30f12559e8c71d33e4cf6ec5dacf465
dyhhhhh <63825522+dyhhhhhh@users.noreply.github.com>
2024-04-30 18:53:43 +0800

    Update test.java

--- read_gk/src/test/java/test.java
+++ read_gk/src/test/java/test.java
@@ -56,36 +56,7 @@ public class test {
 
         //context.executeStrategy();
     }
-    @Test
-    public void test() throws IOException {
-        OkHttpClient client = new OkHttpClient().newBuilder()
-                .build();
-        MediaType mediaType = MediaType.parse("application/json; charset=UTF-8");
-        RequestBody body = RequestBody.create(mediaType, "{\"org_id\":30000000001,\"user_id\":\"30001011626\",\"course_id\":\"30000083529\",\"enrollment_role\":\"student\",\"is_teacher\":false,\"activity_id\":30004402194,\"activity_type\":\"page\",\"activity_name\":null,\"module\":null,\"action\":\"open\",\"ts\":1714437432508,\"user_agent\":\"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0\",\"mode\":\"normal\",\"channel\":\"web\",\"target_info\":{},\"master_course_id\":135717,\"org_name\":\"河北开放大学\",\"org_code\":\"130\",\"user_no\":\"2413001200455\",\"user_name\":\"段雨寒\",\"course_code\":\"202403-02970130\",\"course_name\":\"国家开放大学学习指南\",\"dep_id\":\"30000000002\",\"dep_name\":\"开放教育学院\",\"dep_code\":\"1300000\"}");
-        Request request = new Request.Builder()
-                .url("https://lms.ouchn.cn/statistics/api/learning-activity")
-                .method("POST", body)
-                .addHeader("accept", "application/json, text/javascript, */*; q=0.01")
-                .addHeader("accept-language", "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6")
-                .addHeader("cache-control", "no-cache")
-                .addHeader("content-type", "application/json; charset=UTF-8")
-                .addHeader("cookie", "HWWAFSESID=6f3532fb8d6a4689d20; HWWAFSESTIME=1714432120831; session=V2-30000000001-c4a1aa77-9c26-46cd-a627-26035b44e783.MzAwMDEwMTE2MjY.1714523832459.GwISF19ngmstfzvMkrZ0d6FIcZQ")
-                .addHeader("origin", "https://lms.ouchn.cn")
-                .addHeader("pragma", "no-cache")
-                .addHeader("priority", "u=1, i")
-                .addHeader("referer", "https://lms.ouchn.cn/course/30000083529/learning-activity/full-screen")
-                .addHeader("sec-ch-ua", "\"Microsoft Edge\";v=\"125\", \"Chromium\";v=\"125\", \"Not.A/Brand\";v=\"24\"")
-                .addHeader("sec-ch-ua-mobile", "?0")
-                .addHeader("sec-ch-ua-platform", "\"Windows\"")
-                .addHeader("sec-fetch-dest", "empty")
-                .addHeader("sec-fetch-mode", "cors")
-                .addHeader("sec-fetch-site", "same-origin")
-                .addHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36 Edg/125.0.0.0")
-                .addHeader("x-requested-with", "XMLHttpRequest")
-                .build();
-        Response response = client.newCall(request).execute();
-        System.out.println(response);
-    }
+   
     @Test
     public void jsoupTest() throws IOException {
         System.out.println(CommonApis.get_master_course_id("30000083529"));
