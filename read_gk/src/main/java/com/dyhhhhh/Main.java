b2b9130a8517792d17fb522d913af584baa07d89
dyhhhhh <63825522+dyhhhhhh@users.noreply.github.com>
2024-10-15 14:25:05 +0800

    Delete read_gk directory

--- read_gk/src/main/java/com/dyhhhhh/Main.java
+++
@@ -1,7 +0,0 @@
-package com.dyhhhhh;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- read_gk/src/main/java/com/dyhhhhh/apis/Strategy.java
+++
@@ -1,8 +0,0 @@
-package com.dyhhhhh.apis;
-
-import java.io.IOException;
-import java.util.HashMap;
-
-public interface Strategy {
-    void execute(String activityId, HashMap<String, Object> activityDetails) throws Exception;
-}
